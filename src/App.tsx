8c2139a35c74c3c1332270fff15ce37b424ebf13
Pedro Figueroa <figueroash@gmail.com>
2024-03-02 11:05:10 +0100

    Simplify logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Clothes from "./pages/Clothes";
 import NoPage from "./pages/NoPage";
-import Electronics from "./pages/Electronics";
-import Miscellaneous from "./pages/Miscellaneous";
-import Furniture from "./pages/Furniture";
-import Shoes from "./pages/Shoes";
 import './App.css';
+import Products from "./components/Products/Products";
 
 function App() {
+  function ProductsWrapper({ category }: { category: number | null }) {
+    const location = useLocation();
+    return <Products key={location.pathname} category={category} />;
+  }
+
   return (
     <div className="main-container">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="clothes" element={<Clothes />} />
-            <Route path="electronics" element={<Electronics />} />
-            <Route path="furniture" element={<Furniture />} />
-            <Route path="shoes" element={<Shoes />} />
-            <Route path="miscellaneous" element={<Miscellaneous />} />
+            <Route index element={<ProductsWrapper category={null} />} />
+            <Route path="clothes" element={<ProductsWrapper category={1} />} />
+            <Route path="electronics" element={<ProductsWrapper category={2} />} />
+            <Route path="furniture" element={<ProductsWrapper category={3} />} />
+            <Route path="shoes" element={<ProductsWrapper category={4} />} />
+            <Route path="miscellaneous" element={<ProductsWrapper category={5} />} />
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Products.module.scss';
-
-interface ProductsProps {
-  products: Product[] | null;
-}
 
 interface Product {
   id: number;
@@ -16,18 +12,52 @@ interface Product {
   images: string[];
 }
 
-const Products: React.FC<ProductsProps> = ({ products }) => {
+interface ProductsProps {
+  category: number | null;
+}
+
+const Products: React.FC<ProductsProps> = ({ category }) => {
+  const [products, setProducts] = useState<Product[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let apiUrl = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=16'
+
+        if( category ) {
+          apiUrl = `https://api.escuelajs.co/api/v1/categories/${category}/products?offset=0&limit=8`;
+        }
+
+        const response = await fetch(apiUrl);
+        const jsonData: Product[] = await response.json();
+        setProducts(jsonData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
   return (
-    <div className={styles["products-container"]}>
-      {products && products.map((product) => (
-        <div className={styles["product"]} key={product.id}>
-          <img className={styles["product__image"]} src={product.images[0]} alt={product.title} />
-          <h4 className={styles["product__title"]}>{product.title}</h4>
-          <div className={styles["product__price"]}>$ {product.price}</div>
-          {/* <div className={styles["product__description"]}>{product.description}</div> */}
-          <div className={styles["product__category"]}>{product.category.name}</div>
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className={styles["products-container"]}>
+          {products && products.map((product) => (
+            <div className={styles["product"]} key={product.id}>
+              <img className={styles["product__image"]} src={product.images[0]} alt={product.title} />
+              <h4 className={styles["product__title"]}>{product.title}</h4>
+              <div className={styles["product__price"]}>$ {product.price}</div>
+              {/* <div className={styles["product__description"]}>{product.description}</div> */}
+              <div className={styles["product__category"]}>{product.category.name}</div>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 };
